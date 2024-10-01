37694ffb47af480ce1a57d7690083aa2939e39cf
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-10-01 21:14:40 +0700

    update:detail movies

--- src/pages/movies/DetailMovie.jsx
+++ src/pages/movies/DetailMovie.jsx
@@ -1,38 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar';
-import avatar from '../../../public/assets/avatar.png';
-import { genreMovies, getDetailMovie } from '../../services/api';
+import { getDetailMovie } from '../../services/api';
 import { useParams } from 'react-router-dom';
 
 const DetailMovie = () => {
   const { id } = useParams();
-  const [detailMovie, setDetailMovie] = useState({});
+  const [movie, setMovie] = useState({});
 
   useEffect(() => {
     getDetailMovie(id).then((data) => {
-      setDetailMovie(data);
-      console.log(data);
+      setMovie(data);
     });
   }, [id]);
-  const baseImageUrl = 'https://image.tmdb.org/t/p/original';
+
+  const baseImageUrl = import.meta.env.VITE_BASEIMGURL;
 
   return (
     <>
       <div>
         <Navbar />
       </div>
-      <div>
+      <div className="relative">
         <div className="w-full">
-          <div className="relative">
-            <img
-              src={`${baseImageUrl}/${detailMovie.backdrop_path}`}
+          <div
+            className="relative h-screen bg-center bg-cover"
+            style={{
+              backgroundImage: `linear-gradient(to top, #121212, rgba(0, 0, 0, 0.2)), url(${baseImageUrl}/${movie.backdrop_path})`,
+            }}
+          >
+            {/* <img
+              src={`${baseImageUrl}/${movie.backdrop_path}`}
               alt=""
-              className="object-cover w-full h-screen"
-            />
-            <div className="absolute inset-0 w-full bg-black/70"></div>
+              className="object-cover w-full h-screen bg-gradient-to-t from-black to-transparent"
+            /> */}
+            <div className="absolute inset-0 w-full "></div>
+            <div className="absolute inset-0 flex items-center ml-10">
+              <div className="flex-none">
+                <img
+                  src={`${baseImageUrl}/${movie.poster_path}`}
+                  alt=""
+                  className="w-[300px] h-[400px] object-cover rounded-lg"
+                />
+              </div>
+              <div className="max-w-lg ml-10 text-white">
+                <h1 className="text-4xl font-bold">{movie.title}</h1>
+                <span>{movie.release_date}</span>|
+                <span>
+                  {movie.vote_average ? movie.vote_average.toFixed(1) : 'N/A'}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="container mx-auto text-white">{detailMovie.title}</div>
+
+        <div className="container mx-auto text-white">
+          <div>
+            sasa Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Explicabo libero atque voluptas labore illo, nesciunt nemo ducimus
+            quia nostrum fuga, laboriosam eum nam praesentium repellendus?
+            Debitis molestias sed deserunt cum enim maiores, pariatur dolorem
+            dicta, et harum cupiditate. Illum nemo laborum similique voluptates
+            sequi a veniam dolorum at alias commodi omnis quam, facilis neque
+            molestias ea repellendus molestiae consectetur voluptas beatae quas
+            eum? Blanditiis laborum error, voluptatibus magnam placeat provident
+            amet itaque facere minima repellat quae numquam facilis tempora
+            eligendi minus nemo alias tenetur. Aliquid, veritatis? Distinctio,
+            mollitia nostrum? Recusandae laboriosam assumenda non voluptatem,
+            magnam doloribus quis fugit minus culpa?
+          </div>
+        </div>
       </div>
     </>
   );
