87922ec24df789ce2751b1058bac7b67eaa09677
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2025-01-05 20:06:34 +0700

    update:date

--- src/pages/movies/AllMovies.jsx
+++ src/pages/movies/AllMovies.jsx
@@ -4,6 +4,12 @@ import { RiSlideshow3Fill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 import NowPlayingSlider from '../home/_partials/NowPlayingSlider';
 import Navbar from '../../components/Navbar';
+import PopularMoviesSlider from '../home/_partials/PopularMoviesSlider';
+import { FaFire } from 'react-icons/fa';
+import { MdUpcoming } from 'react-icons/md';
+import UpcomingMoviesSlider from '../home/_partials/UpcomingMoviesSlider';
+import TopRatedMoviesSlider from '../home/_partials/TopRatedMoviesSlider';
+import { IoIosStar } from 'react-icons/io';
 
 const AllMovies = () => {
   return (
@@ -28,6 +34,61 @@ const AllMovies = () => {
           </div>
           <div>
             <NowPlayingSlider />
+          </div>
+        </div>
+        <div className="my-20">
+          <div className="flex items-center justify-between my-5">
+            <p className="flex items-center gap-2 text-base font-bold text-white md:text-2xl">
+              <FaFire /> Popular Movies
+            </p>
+            <Link to={'/movies/popular-movies'}>
+              <Button
+                size="sm"
+                className="text-white capitalize rounded-none bg-primary"
+              >
+                View All
+              </Button>
+            </Link>
+          </div>
+          <div>
+            <PopularMoviesSlider />
+          </div>
+        </div>
+        <div className="my-20">
+          <div className="flex items-center justify-between my-5">
+            <p className="flex items-center gap-2 text-base font-bold text-white md:text-2xl">
+              <IoIosStar /> Top Rated Movies
+            </p>
+            <Link to={'/movies/top-rated-movies'}>
+              <Button
+                size="sm"
+                className="text-white capitalize rounded-none bg-primary"
+              >
+                View All
+              </Button>
+            </Link>
+          </div>
+          <div>
+            <TopRatedMoviesSlider />
+          </div>
+        </div>
+
+        <div className="my-20">
+          <div className="flex items-center justify-between my-5">
+            <p className="flex items-center gap-2 text-base font-bold text-white md:text-2xl">
+              <MdUpcoming /> Upcoming Movies
+            </p>
+            <Link to={'/movies/upcoming-movies'}>
+              <Button
+                size="sm"
+                className="text-white capitalize rounded-none bg-primary"
+              >
+                View All
+              </Button>
+            </Link>
+          </div>
+          <div>
+            <UpcomingMoviesSlider />
           </div>
         </div>
       </section>
--- src/utils/dateUtils.jsx
+++ src/utils/dateUtils.jsx
@@ -1,5 +1,8 @@
 export const formatDate = (dateString) => {
   const date = new Date(dateString);
+  if (isNaN(date)) {
+    return 'Invalid Date';
+  }
   return new Intl.DateTimeFormat('en-US', {
     month: 'long',
     day: 'numeric',
